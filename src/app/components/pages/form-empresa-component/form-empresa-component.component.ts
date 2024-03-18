d49eddb04352c2b3c6a4435a823d957ceb81887e
devfilipealmeida <filipesilva504@gmail.com>
2024-03-18 20:24:57 -0300

    otimização na função de submit e populate form ao editar

--- src/app/components/pages/form-empresa-component/form-empresa-component.component.ts
+++ src/app/components/pages/form-empresa-component/form-empresa-component.component.ts
@@ -5,7 +5,8 @@ import { MessagesService } from '../../../services/messages.service';
 import { Company } from '../../../interfaces/Empresa';
 import { CommonModule } from '@angular/common';
 import { NgxMaskDirective } from 'ngx-mask';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { adjustDateFormat, dateToString } from '../../../utils/js_functions';
 
 @Component({
   selector: 'app-form-empresa-component',
@@ -15,26 +16,16 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrl: './form-empresa-component.component.css'
 })
 export class FormEmpresaComponentComponent implements OnInit{
+  form!: FormGroup;
   isEditMode = false;
   empresaId: number = 0;
-  empresa: Company = {
-    responsible_company: '',
-    cpf: '',
-    birth_date: '',
-    fantasy_name: '',
-    cnpj: '',
-    address: '',
-    neighborhood: '',
-    complement: '',
-    city: '',
-    state: ''
-  };
-
+  
   constructor(
     private router: Router, 
     private route: ActivatedRoute, 
     private empresaService: EmpresaService, 
-    private messagesService: MessagesService
+    private messagesService: MessagesService,
+    private formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
@@ -45,6 +36,19 @@ export class FormEmpresaComponentComponent implements OnInit{
         this.getById(params.get('id'));
       }
     });
+
+    this.form = this.formBuilder.group({
+      responsible_company: ['', Validators.required],
+      cpf: ['', Validators.required],
+      birth_date: ['', Validators.required],
+      fantasy_name: ['', Validators.required],
+      cnpj: ['', Validators.required],
+      address: ['', Validators.required],
+      neighborhood: ['', Validators.required],
+      complement: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required]
+    });
   }
 
   backToHome() {
@@ -54,82 +58,43 @@ export class FormEmpresaComponentComponent implements OnInit{
   submitForm(event: Event): void {
     event.preventDefault();
 
-    const responsible_company = (document.getElementById('responsible_company') as HTMLInputElement).value;
-    const cpf = (document.getElementById('cpf') as HTMLInputElement).value;
-    const birth_date = (document.getElementById('birth_date') as HTMLInputElement).value;
-    const fantasy_name = (document.getElementById('fantasy_name') as HTMLInputElement).value;
-    const cnpj = (document.getElementById('cnpj') as HTMLInputElement).value;
-    const address = (document.getElementById('address') as HTMLInputElement).value;
-    const neighborhood = (document.getElementById('neighborhood') as HTMLInputElement).value;
-    const complement = (document.getElementById('complement') as HTMLInputElement).value;
-    const city = (document.getElementById('city') as HTMLInputElement).value;
-    const state = (document.getElementById('state') as HTMLInputElement).value;
+    const formValues = this.form.value;
 
-    const data = {
-      responsible_company,
-      cpf,
-      birth_date,
-      fantasy_name,
-      cnpj: cnpj.replace(/\D/g, ''),
-      address,
-      neighborhood,
-      complement,
-      city,
-      state
-    };
+    formValues.cpf = formValues.cpf.replace(/\D/g, '');
+    formValues.cnpj = formValues.cnpj.replace(/\D/g, '');
+    formValues.birth_date = adjustDateFormat(formValues.birth_date)
 
     if(this.isEditMode) {
-      this.empresaService.updateEmpresa(data, this.empresaId).subscribe();
-  
-      this.messagesService.add('Empresa atualizada com sucesso.');
-  
-      this.router.navigate(['home']);
+      this.empresaService.updateEmpresa(formValues, this.empresaId).subscribe(() => {
+        this.messagesService.add('Empresa atualizada com sucesso.');
+        this.router.navigate(['home']);
+      });
     } else {
-      this.empresaService.createEmpresa(data).subscribe();
-  
-      this.messagesService.add('Empresa cadastrada com sucesso.');
-  
-      this.router.navigate(['home']);
+      this.empresaService.createEmpresa(formValues).subscribe(() => {
+        this.messagesService.add('Empresa cadastrada com sucesso.');
+        this.router.navigate(['home']);
+      });
     }
-
   }
 
   getById(id: number) {
     this.empresaService.getById(id).subscribe((empresa) => {
-      this.empresa = empresa;
-      this.populateFormFields();
+      this.populateFormFields(empresa);
     });
   }
 
-  populateFormFields() {
-    const responsible_company = document.getElementById('responsible_company') as HTMLInputElement;
-    const cpf = document.getElementById('cpf') as HTMLInputElement;
-    const birth_date = document.getElementById('birth_date') as HTMLInputElement;
-    const fantasy_name = document.getElementById('fantasy_name') as HTMLInputElement;
-    const cnpj = document.getElementById('cnpj') as HTMLInputElement;
-    const address = document.getElementById('address') as HTMLInputElement;
-    const neighborhood = document.getElementById('neighborhood') as HTMLInputElement;
-    const complement = document.getElementById('complement') as HTMLInputElement;
-    const city = document.getElementById('city') as HTMLInputElement;
-    const state = document.getElementById('state') as HTMLInputElement;
-
-    responsible_company.value = this.empresa.fantasy_name;
-    cpf.value = this.empresa.cpf;
-    fantasy_name.value = this.empresa.fantasy_name;
-    cnpj.value = this.empresa.cnpj;
-    address.value = this.empresa.address;
-    neighborhood.value = this.empresa.neighborhood,
-    complement.value = this.empresa.complement,
-    city.value = this.empresa.city;
-    state.value = this.empresa.state;
-    
-
-    const stringDate = new Date(this.empresa.birth_date);
-    const dia = stringDate.getDate().toString().padStart(2, '0');
-    const mes = (stringDate.getMonth() + 1).toString().padStart(2, '0');
-    const ano = stringDate.getFullYear().toString();
-    const dataFormatada = `${dia}/${mes}/${ano}`;
-
-    birth_date.value = dataFormatada;
-  }
+  populateFormFields(empresa: Company) {
+    this.form.patchValue({
+      responsible_company: empresa.responsible_company,
+      cpf: empresa.cpf,
+      birth_date: dateToString(empresa.birth_date),
+      fantasy_name: empresa.fantasy_name,
+      cnpj: empresa.cnpj,
+      address: empresa.address,
+      neighborhood: empresa.neighborhood,
+      complement: empresa.complement,
+      city: empresa.city,
+      state: empresa.state
+    });
+  } 
 }
