696cd2ead2ee2f80721a42b66bbc6266063449eb
devfilipealmeida <filipesilva504@gmail.com>
2024-03-17 22:03:54 -0300

    import de header e tabela de empresas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+import { TableComponentComponent } from './components/table-component/table-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponentComponent,
+    TableComponentComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
