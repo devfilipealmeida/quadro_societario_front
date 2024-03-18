b945d5a947660519ce143e2d68d7c92b63d69ad8
devfilipealmeida <filipesilva504@gmail.com>
2024-03-18 01:50:32 -0300

    implementação do roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { TableComponentComponent } from './components/pages/table-component/table-component.component';
+import { FormEmpresaComponentComponent } from './components/pages/form-empresa-component/form-empresa-component.component';
+import { FormSocioComponentComponent } from './components/pages/form-socio-component/form-socio-component.component';
+import { DetalhesEmpresaComponentComponent } from './components/pages/detalhes-empresa-component/detalhes-empresa-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: TableComponentComponent },
+    { path: 'home', component: TableComponentComponent },
+    { path: 'cadastro/empresa', component: FormEmpresaComponentComponent },
+    { path: 'cadastro/socio', component: FormSocioComponentComponent },
+    { path: 'detalhes/empresa', component: DetalhesEmpresaComponentComponent },
+];
