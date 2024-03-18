11dec5874ed4c441148d009ca3c32fc318dc41a3
devfilipealmeida <filipesilva504@gmail.com>
2024-03-18 00:04:29 -0300

    testando a tabela com mock

--- src/app/components/table-component/table-component.component.ts
+++ src/app/components/table-component/table-component.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-table-component',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,],
   templateUrl: './table-component.component.html',
   styleUrl: './table-component.component.css'
 })
 export class TableComponentComponent {
-
+  empresas = [
+    { responsavel: 'João Cirino', nomeFantasia: 'Tectoy Indústria de Eletrônicos', cnpj: '33.942.192-0001/04' },
+    { responsavel: 'Filipe Almeida', nomeFantasia: 'Fatech Tecnologia', cnpj: '33.942.192-0001/05' },
+    { responsavel: 'Noah Almeida', nomeFantasia: 'Noahzin Eletrônics Industries', cnpj: '33.942.192-0001/06' }
+  ];
 }
