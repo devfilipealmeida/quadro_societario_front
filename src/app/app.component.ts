876d1591955821f5882a3ad0f0feaa7bfaf00d80
devfilipealmeida <filipesilva504@gmail.com>
2024-03-18 01:54:15 -0300

    importações dos novos componetes e bind no projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
-import { TableComponentComponent } from './components/table-component/table-component.component';
+import { TableComponentComponent } from './components/pages/table-component/table-component.component';
+import { FormEmpresaComponentComponent } from './components/pages/form-empresa-component/form-empresa-component.component';
+import { FormSocioComponentComponent } from './components/pages/form-socio-component/form-socio-component.component';
+import { DetalhesEmpresaComponentComponent } from './components/pages/detalhes-empresa-component/detalhes-empresa-component.component';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +13,10 @@ import { TableComponentComponent } from './components/table-component/table-comp
     RouterOutlet,
     HeaderComponentComponent,
     TableComponentComponent,
+    FormEmpresaComponentComponent,
+    FormSocioComponentComponent,
+    DetalhesEmpresaComponentComponent,
+    RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
