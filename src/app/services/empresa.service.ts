03278c82963ce7e8e106a6c97b45f35312044624
devfilipealmeida <filipesilva504@gmail.com>
2024-03-18 22:41:50 -0300

    ajuste dos services com as variáveis de ambiente

--- src/app/services/empresa.service.ts
+++ src/app/services/empresa.service.ts
@@ -2,32 +2,34 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Company, Empresa } from '../interfaces/Empresa';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpresaService {
-  private apiUrl = 'http://127.0.0.1:8000/api'
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}/api/corporations`;
 
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Empresa> {
-    return this.http.get<Empresa>(`${this.apiUrl}/corporations`);
+    return this.http.get<Empresa>(this.apiUrl);
   }
 
   getEmpresaById(id: number): Observable<Company> {
-    return this.http.get<Company>(`${this.apiUrl}/corporations/${id}`);
+    return this.http.get<Company>(`${this.apiUrl}/${id}`);
   }
 
   createEmpresa(data: Company): Observable<Company> {
-    return this.http.post<Company>(`${this.apiUrl}/corporations`, data);
+    return this.http.post<Company>(this.apiUrl, data);
   }
 
   getById(id: number): Observable<Company> {
-    return this.http.get<Company>(`${this.apiUrl}/corporations/${id}`);
+    return this.http.get<Company>(`${this.apiUrl}/${id}`);
   }
 
   updateEmpresa(data: Company, empresaId: number): Observable<Company> {
-    return this.http.put<Company>(`${this.apiUrl}/corporations/${empresaId}`, data);
+    return this.http.put<Company>(`${this.apiUrl}/${empresaId}`, data);
   }
 }
--- src/app/services/socio.service.ts
+++ src/app/services/socio.service.ts
@@ -2,30 +2,32 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Socio } from '../interfaces/Socio';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocioService {
-  private apiUrl = 'http://127.0.0.1:8000/api'
+  private baseApiUrl = environment.baseApiUrl;
+  private apiUrl = `${this.baseApiUrl}/api/partners`;
 
   constructor(private http: HttpClient) { }
 
   createSocio(data: Socio): Observable<Socio> {
     console.log('service', data)
-    return this.http.post<Socio>(`${this.apiUrl}/partners`, data);
+    return this.http.post<Socio>(this.apiUrl, data);
   }
 
   deleteByCpf(cpf: string): Observable<string> {
-    return this.http.delete<string>(`${this.apiUrl}/partners/remove/cpf/${cpf}`);
+    return this.http.delete<string>(`${this.apiUrl}/remove/cpf/${cpf}`);
   }
 
   getByCpf(cpf: string): Observable<Socio> {
-    return this.http.get<Socio>(`${this.apiUrl}/partners/cpf/${cpf}`);
+    return this.http.get<Socio>(`${this.apiUrl}/cpf/${cpf}`);
   }
 
   updateSocio(data: Socio): Observable<Socio> {
     const cpfToUrl = String(data.cpf);
-    return this.http.put<Socio>(`${this.apiUrl}/partners/${cpfToUrl}`, data);
+    return this.http.put<Socio>(`${this.apiUrl}/${cpfToUrl}`, data);
   }
 }
