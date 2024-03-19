aed3996fa48c31a69eb31fa4e86148f27c03b4da
devfilipealmeida <filipesilva504@gmail.com>
2024-03-19 00:22:08 -0300

    ajuste na função que estava retornando um dia a menos

--- src/app/utils/js_functions.ts
+++ src/app/utils/js_functions.ts
@@ -7,9 +7,9 @@ export function adjustDateFormat(birthDate: string): string {
 
   export function dateToString(birthDate: Date | string) {
     const stringDate = new Date(birthDate);
-    const dia = stringDate.getDate().toString().padStart(2, '0');
-    const mes = (stringDate.getMonth() + 1).toString().padStart(2, '0');
-    const ano = stringDate.getFullYear().toString();
+    const dia = stringDate.getUTCDate().toString().padStart(2, '0');
+    const mes = (stringDate.getUTCMonth() + 1).toString().padStart(2, '0');
+    const ano = stringDate.getUTCFullYear().toString();
     const dataFormatada = `${dia}/${mes}/${ano}`;
     return dataFormatada;
   }
